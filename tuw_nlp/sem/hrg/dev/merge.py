6cbed2dff04e756e7b386096da53f1b4ecca9433
Eszti <eszter.iklodi@gmail.com>
2024-07-09 13:20:18 +0200

    Enable top k for merge

--- tuw_nlp/sem/hrg/dev/merge.py
+++ tuw_nlp/sem/hrg/dev/merge.py
@@ -13,10 +13,11 @@ def get_args():
     parser.add_argument("-o", "--out-file", type=str)
     parser.add_argument("-f", "--first", type=int)
     parser.add_argument("-l", "--last", type=int)
+    parser.add_argument("-k", type=int)
     return parser.parse_args()
 
 
-def main(in_dir, out_fn, first, last):
+def main(in_dir, out_fn, first, last, k):
     all_ex_set = defaultdict(set)
 
     ex_stat = Counter()
@@ -34,24 +35,33 @@ def main(in_dir, out_fn, first, last):
             extractions = json.load(f)
         assert len(extractions.keys()) == 1
         sen = list(extractions.keys())[0]
-        for ex in extractions[sen]:
+        top_k = extractions[sen]
+        if k:
+            top_k = top_k[:k]
+        for ex in top_k:
             wire_ex = WiReEx(ex)
             all_ex_set[sen].add(wire_ex)
-        ex_stat[len(extractions[sen])] += 1
+        ex_stat[len(top_k)] += 1
 
     all_ex_list = {}
     for sen, items in all_ex_set.items():
         all_ex_list[sen] = list(items)
     with open(out_fn, "w") as f:
         json.dump(all_ex_list, f, indent=4)
+
+    if k:
+        print(f"\nKeeping top {k} extractions.")
+    else:
+        print(f"\nKeeping all extractions.")
     sum = 0
     for k, v in ex_stat.items():
         print(f"{k} extraction(s): {v} sen")
         sum += v
-    print(f"Sum {sum} sens")
+    print(f"Sum {sum} sens\n")
+
     print(f"Output saved to {out_fn}")
 
 
 if __name__ == "__main__":
     args = get_args()
-    main(args.in_dir, args.out_file, args.first, args.last)
+    main(args.in_dir, args.out_file, args.first, args.last, args.k)
