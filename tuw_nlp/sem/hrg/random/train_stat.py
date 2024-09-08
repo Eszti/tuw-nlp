985da5625a54a6037c922694d041f5cbca44214b
Eszti <eszter.iklodi@gmail.com>
2024-09-08 23:36:52 +0200

    Save train stat under random

--- tuw_nlp/sem/hrg/random/train_stat.py
+++ tuw_nlp/sem/hrg/random/train_stat.py
@@ -9,19 +9,17 @@ from tuw_nlp.text.utils import gen_tsv_sens
 def get_args():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("-t", "--train-fn", type=str)
-    parser.add_argument("-o1", "--out-seq", type=str)
-    parser.add_argument("-o2", "--out-nr-ex", type=str)
+    parser.add_argument("-o", "--out-dir", type=str)
     return parser.parse_args()
 
 
-def main(train_fn, out_seq, out_nr_ex):
+def main(train_fn, out_dir):
     seq_stat = defaultdict(list)
     nr_ex_stat = Counter()
     last_sen_txt = ""
     cnt = 1
     with open(train_fn) as f:
         for sen_idx, sen in enumerate(gen_tsv_sens(f)):
-            print(f"Processing sen {sen_idx}")
             labels_seq = get_labels_str(sen)
             seq_stat[len(sen)].append(labels_seq)
             sen_txt = " ".join([word[1] for word in sen])
@@ -33,12 +31,12 @@ def main(train_fn, out_seq, out_nr_ex):
             last_sen_txt = sen_txt
     print(f"stat len: {len(seq_stat)}")
     print(f"{sorted(seq_stat)}")
-    with open(out_seq, "w") as f:
-        json.dump(seq_stat, f)
-    with open(out_nr_ex, "w") as f:
-        json.dump(nr_ex_stat, f)
+    with open(f"{out_dir}/train_seq_dist.json", "w") as f:
+        json.dump({key: v for key, v in sorted(seq_stat.items())}, f)
+    with open(f"{out_dir}/train_nr_ex.json", "w") as f:
+        json.dump({key: v for key, v in sorted(nr_ex_stat.items())}, f)
 
 
 if __name__ == "__main__":
     args = get_args()
-    main(args.train_fn, args.out_seq, args.out_nr_ex)
+    main(args.train_fn, args.out_dir)
