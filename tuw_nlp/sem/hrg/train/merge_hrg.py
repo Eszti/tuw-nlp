27f91156138f48ce84e29a58b92279871e7a11ad
Eszti <eszter.iklodi@gmail.com>
2024-09-08 23:17:17 +0200

    Refactor merge_hrg and regenerate grammars * weights always sum up to 1 for one non-terminal

--- tuw_nlp/sem/hrg/train/merge_hrg.py
+++ tuw_nlp/sem/hrg/train/merge_hrg.py
@@ -11,6 +11,54 @@ def get_args():
     return parser.parse_args()
 
 
+def cut_grammar(grammar, size):
+    all_prods = get_total(grammar)
+    factor = size / all_prods
+    new_grammar = defaultdict(Counter)
+    for nt, prods in grammar.items():
+        for prod, cnt in prods.most_common(n=int(round(factor*len(prods)))):
+            new_grammar[nt][prod] = cnt
+    return new_grammar
+
+
+def add_weights(grammar):
+    new_grammar = defaultdict(list)
+    for nt, prods in grammar.items():
+        for prod, cnt in prods.most_common():
+            w = round(cnt / prods.total(), 2)
+            if w < 0.01:
+                w = 0.01
+            new_grammar[nt].append((prod, cnt, w))
+    return new_grammar
+
+
+def write_rules(f, grammar, numeric_info=None):
+    for (prod, cnt, w) in grammar["S"]:
+        if not numeric_info:
+            f.write(f"{prod}\n")
+        elif numeric_info == "cnt":
+            f.write(f"{prod}\t{cnt}\n")
+        elif numeric_info == "weight":
+            f.write(f"{prod}\t{w}\n")
+    for nt, prods in grammar.items():
+        if nt == "S":
+            continue
+        for (prod, cnt, w) in prods:
+            if not numeric_info:
+                f.write(f"{prod}\n")
+            elif numeric_info == "cnt":
+                f.write(f"{prod}\t{cnt}\n")
+            elif numeric_info == "weight":
+                f.write(f"{prod}\t{w}\n")
+
+
+def get_total(grammar):
+    all_prods = 0
+    for nt, prods in grammar.items():
+        all_prods += len(prods)
+    return all_prods
+
+
 def main(input_dir, out_dir, size):
     grammar = defaultdict(Counter)
     for sen_dir in os.listdir(input_dir):
@@ -20,53 +68,19 @@ def main(input_dir, out_dir, size):
                 lines = f.readlines()
             for line in lines:
                 rule = line.strip()
-                nt = rule[0]
+                nt = line.split(" ")[0]
                 grammar[nt][rule] += 1
+    if size:
+        grammar = cut_grammar(grammar, size)
+    grammar = add_weights(grammar)
     size_str = str(size) if size is not None else "all"
     with open(f"{out_dir}/grammar_{size_str}.hrg", "w") as f:
-        write_rule(f, grammar, "weight", size)
+        write_rules(f, grammar, "weight")
     with open(f"{out_dir}/grammar_{size_str}.stat", "w") as f:
-        write_rule(f, grammar, "cnt", size)
+        write_rules(f, grammar, "cnt")
     print(f"Unique rules: {get_total(grammar)}")
     for nt, prods in grammar.items():
-        print(f"{nt}: {len(prods)}")
-
-
-def write_rule(f, grammar, numeric_info=None, size=None):
-    factor = 1
-    if size:
-        all_prods = get_total(grammar)
-        factor = size / all_prods
-    for prod, cnt in grammar["S"].most_common(n=int(round(factor*len(grammar["S"])))):
-        if not numeric_info:
-            f.write(f"{prod}\n")
-        elif numeric_info == "cnt":
-            f.write(f"{prod}\t{cnt}\n")
-        elif numeric_info == "weight":
-            w = round(cnt / grammar["S"].total(), 2)
-            if w < 0.01:
-                w = 0.01
-            f.write(f"{prod}\t{w}\n")
-    for nt, prods in grammar.items():
-        if nt == "S":
-            continue
-        for prod, cnt in prods.most_common(n=int(round(factor*len(prods)))):
-            if not numeric_info:
-                f.write(f"{prod}\n")
-            elif numeric_info == "cnt":
-                f.write(f"{prod}\t{cnt}\n")
-            elif numeric_info == "weight":
-                w = round(cnt / prods.total(), 2)
-                if w < 0.01:
-                    w = 0.01
-                f.write(f"{prod}\t{w}\n")
-
-
-def get_total(grammar):
-    all_prods = 0
-    for nt, prods in grammar.items():
-        all_prods += len(prods)
-    return all_prods
+        print(f"{nt}: {len(prods)}\t({round(len(prods)/get_total(grammar), 3)})")
 
 
 if __name__ == "__main__":
