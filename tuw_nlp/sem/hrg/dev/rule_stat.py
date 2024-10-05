97cdcae9a4602dcd9990098bd3111764a941876e
Eszti <eszter.iklodi@gmail.com>
2024-10-06 00:25:34 +0200

    Add rule usage correlation with rule weights

--- tuw_nlp/sem/hrg/dev/rule_stat.py
+++ tuw_nlp/sem/hrg/dev/rule_stat.py
@@ -3,6 +3,7 @@ import json
 import os
 from collections import OrderedDict
 
+import numpy as np
 import pandas as pd
 
 from tuw_nlp.sem.hrg.common.io import get_range
@@ -54,19 +55,31 @@ def main(data_dir, config_json):
         for sen_dir in get_range(f"{data_dir}/{c['in_dir']}", first, last):
             for bf in c["bolinas_filters"]:
                 fill_stat(bf, c, data_dir, sen_dir, stat_dict)
+
         stat_dict = OrderedDict(sorted(stat_dict.items()))
         complete_rules, lhs, weights = zip(*rules)
+
         df_abs = pd.DataFrame(stat_dict, index=range(1, len(rules)+1))
         df_rel = df_abs.apply(lambda x: x*100 / x.sum()).map('{:.4}'.format)
         add_rule_cols(df_abs, complete_rules, lhs, weights)
         df_abs.to_csv(f"{report_dir}/{c['in_dir']}_abs.tsv", sep="\t")
         add_rule_cols(df_rel, complete_rules, lhs, weights)
         df_rel.to_csv(f"{report_dir}/{c['in_dir']}_rel.tsv", sep="\t")
+
+        corr_lines = []
         for name, group in df_abs.groupby("lhs"):
+            group.to_csv(f"{report_dir}/{c['in_dir']}_abs_{name}.tsv", sep="\t")
             df_lhs = group.iloc[:, -len(stat_dict):].apply(lambda x: x*100 / x.sum()).map('{:.4}'.format)
             add_rule_cols(df_lhs, group["rule"].to_list(), group["lhs"].to_list(), group["weight"].to_list())
             df_lhs.to_csv(f"{report_dir}/{c['in_dir']}_rel_{name}.tsv", sep="\t")
-            group.to_csv(f"{report_dir}/{c['in_dir']}_abs_{name}.tsv", sep="\t")
+
+            corr_lines.append(f"{name}\n")
+            corr_cols = ["weight"] + list(stat_dict.keys())
+            corr_lines.append(f"{corr_cols}\n")
+            corr = np.corrcoef(group[corr_cols], rowvar=False)
+            corr_lines.append(f"{corr}\n\n")
+        with open(f"{report_dir}/{c['in_dir']}_corr.txt", "w") as f:
+            f.writelines(corr_lines)
 
 
 def add_rule_cols(df, complete_rules, lhs, weights):
