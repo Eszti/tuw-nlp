7735d7da08e2e3cbc7db20a8f1df71a99f198555
Eszti <eszter.iklodi@gmail.com>
2024-12-11 22:19:25 +0100

    Restructure saved dot graphs

--- tuw_nlp/sem/hrg/common/io.py
+++ tuw_nlp/sem/hrg/common/io.py
@@ -1,14 +1,3 @@
-from stanza.utils.conll import CoNLL
-
-
-def parse_doc(nlp, sen, out_dir, save=True):
-    parsed_doc = nlp(" ".join(t[1] for t in sen))
-    if save:
-        fn = f"{out_dir}/parsed.conll"
-        CoNLL.write_doc2conll(parsed_doc, fn)
-    return parsed_doc
-
-
 def save_bolinas_str(fn, graph, add_names=False):
     bolinas_graph = graph.to_bolinas(add_names=add_names)
     with open(fn, "w") as f:
@@ -18,8 +7,3 @@ def save_bolinas_str(fn, graph, add_names=False):
 def save_as_dot(fn, graph):
     with open(fn, "w") as f:
         f.write(graph.to_dot())
-
-
-def log_to_console_and_log_lines(line, sen_log_lines):
-    print(line)
-    sen_log_lines.append(f"{line}\n")
--- tuw_nlp/sem/hrg/steps/preproc/preproc.py
+++ tuw_nlp/sem/hrg/steps/preproc/preproc.py
@@ -3,54 +3,12 @@ from collections import defaultdict
 
 import networkx as nx
 import stanza
+from stanza.utils.conll import CoNLL
 
 from tuw_nlp.graph.ud_graph import UDGraph
-from tuw_nlp.sem.hrg.common.io import parse_doc, save_bolinas_str, save_as_dot
+from tuw_nlp.sem.hrg.common.io import save_bolinas_str, save_as_dot
 from tuw_nlp.sem.hrg.common.script.loop_on_conll import LoopOnConll
 from tuw_nlp.sem.hrg.common.triplet import Triplet
-
-
-def get_triplet(sen):
-    triplet_dict = defaultdict(list)
-    for i, tok in enumerate(sen):
-        label = tok[7].split("-")[0]
-        if label == "P" or label.startswith("A"):
-            triplet_dict[label].append(i + 1)
-    return Triplet(triplet_dict)
-
-
-def get_triplet_subgraph(ud_graph, triplet):
-    idx_to_keep = list(triplet.node_to_label.keys())
-    return ud_graph.subgraph(idx_to_keep, handle_unconnected="shortest_path").pos_edge_graph()
-
-
-def add_triplet_data_to_node_name(graph, triplet, node_prefix=""):
-    for n in graph.G.nodes:
-        key = n
-        if node_prefix:
-            key = n.split(node_prefix)[1]
-        label = triplet.get_label(key)
-        if label:
-            new_name = graph.G.nodes[n]["name"]
-            if new_name:
-                new_name += "\n"
-            new_name += f"{label}"
-            graph.G.nodes[n]["name"] = new_name
-
-
-def save_conll(sen, fn):
-    with open(fn, 'w') as f:
-        for line in sen:
-            line[0] = str(int(line[0]) + 1)
-            f.write("\t".join(line))
-            f.write("\n")
-
-
-def add_node_labels(bolinas_graph):
-    for node, data in bolinas_graph.G.nodes(data=True):
-        name = data['name']
-        if not name:
-            data["name"] = node
 
 
 class Preproc(LoopOnConll):
@@ -66,9 +24,9 @@ class Preproc(LoopOnConll):
         )
 
     def _do_for_sen(self, sen_idx, sen, sen_txt, last_sen_txt, sen_dir):
-        save_conll(sen, f"{sen_dir}/sen{sen_idx}.conll")
-        parsed_doc = parse_doc(self.nlp, sen, sen_dir, save=sen_txt != last_sen_txt)
-        triplet = get_triplet(sen)
+        self._save_conll(sen, f"{sen_dir}/sen{sen_idx}.conll")
+        parsed_doc = self._parse_doc(self.nlp, sen, sen_dir, save=sen_txt != last_sen_txt)
+        triplet = self._get_triplet(sen)
         triplet.to_file(f"{sen_dir}/sen{sen_idx}_triplet.txt")
         ud_graph = UDGraph(parsed_doc.sentences[0])
 
@@ -81,17 +39,67 @@ class Preproc(LoopOnConll):
             bolinas_graph = ud_graph.pos_edge_graph()
             save_bolinas_str(f"{sen_dir}/pos_edge.graph", bolinas_graph)
             save_bolinas_str(f"{sen_dir}/pos_edge_with_labels.graph", bolinas_graph, add_names=True)
-            add_node_labels(bolinas_graph)
+            self._add_node_labels(bolinas_graph)
             save_as_dot(f"{sen_dir}/pos_edge_graph.dot", bolinas_graph)
 
-        triplet_subgraph = get_triplet_subgraph(ud_graph, triplet)
-        save_as_dot(f"{sen_dir}/sen{sen_idx}_triplet_graph.dot", triplet_subgraph)
-        save_bolinas_str(f"{sen_dir}/sen{sen_idx}_triplet.graph", triplet_subgraph)
+        triplet_ud = ud_graph.subgraph(list(triplet.node_to_label.keys()), handle_unconnected="shortest_path")
+        triplet_pos_edge = triplet_ud.pos_edge_graph()
+        save_bolinas_str(f"{sen_dir}/sen{sen_idx}_triplet.graph", triplet_pos_edge)
+        self._add_node_labels(triplet_pos_edge)
+        save_as_dot(f"{sen_dir}/sen{sen_idx}_triplet_graph.dot", triplet_pos_edge)
 
-        add_triplet_data_to_node_name(ud_graph, triplet)
-        save_as_dot(f"{sen_dir}/sen{sen_idx}_ud.dot", ud_graph)
+        self._add_triplet_data_to_node_name(triplet_ud, triplet)
+        save_as_dot(f"{sen_dir}/sen{sen_idx}_ud.dot", triplet_ud)
+
+    @staticmethod
+    def _get_triplet(sen):
+        triplet_dict = defaultdict(list)
+        for i, tok in enumerate(sen):
+            label = tok[7].split("-")[0]
+            if label == "P" or label.startswith("A"):
+                triplet_dict[label].append(i + 1)
+        return Triplet(triplet_dict)
+
+    @staticmethod
+    def _add_triplet_data_to_node_name(graph, triplet, node_prefix=""):
+        for n in graph.G.nodes:
+            key = n
+            if node_prefix:
+                key = n.split(node_prefix)[1]
+            label = triplet.get_label(key)
+            if label is None:
+                label = "X"
+            new_name = graph.G.nodes[n]["name"]
+            if new_name:
+                new_name += "\n"
+            new_name += f"{label}"
+            graph.G.nodes[n]["name"] = new_name
+
+    @staticmethod
+    def _save_conll(sen, fn):
+        with open(fn, 'w') as f:
+            for line in sen:
+                line[0] = str(int(line[0]) + 1)
+                f.write("\t".join(line))
+                f.write("\n")
+
+    @staticmethod
+    def _add_node_labels(bolinas_graph):
+        for node, data in bolinas_graph.G.nodes(data=True):
+            new_name = str(node)
+            name = data['name']
+            if name:
+                new_name += f"\n{name}"
+            data["name"] = new_name
+
+    @staticmethod
+    def _parse_doc(nlp, sen, out_dir, save=True):
+        parsed_doc = nlp(" ".join(t[1] for t in sen))
+        if save:
+            fn = f"{out_dir}/parsed.conll"
+            CoNLL.write_doc2conll(parsed_doc, fn)
+        return parsed_doc
 
 
 if __name__ == "__main__":
     Preproc().run()
-
